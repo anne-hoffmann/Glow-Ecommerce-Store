703ab619f4218daf2c3c37516c111cca875fc8ff
Anestasia Hoffmann <anestasiahoffmann@gmail.com>
2024-01-19 15:26:47 +0200

    next.config updated

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['localhost'],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
 
-module.exports = {
-    images: {
-      domains: ['localhost'],
-    },
-  };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
+
