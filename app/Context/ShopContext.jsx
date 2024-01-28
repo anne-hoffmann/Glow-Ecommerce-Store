07c1c1e0ed500d777e98d2ad52791f84fc7574fc
Anestasia Hoffmann <anestasiahoffmann@gmail.com>
2024-01-28 18:51:48 +0200

    Context fixed

--- app/Context/ShopContext.jsx
+++ app/Context/ShopContext.jsx
@@ -1,128 +1,115 @@
-"use client"
+"use client";
 import React, { createContext, useEffect, useState } from "react";
-
 
 export const ShopContext = createContext(null);
 
-
 const getDefaultCart = () => {
-    let cart ={};
-    for (let index = 0; index < 300+1; index++){
-        cart[index] = 0;
-    }
-    return cart;
-}
-
-
-const ShopContextProvider = (props) => {
-
-    const [all_products, setAll_Products] = useState([]);
-    const [cartItems, setCartItems] = useState(getDefaultCart());
-
-    fetch('https://glow-server.onrender.com/allproducts')
-    .then((response)=>response.json())
-    .then((data)=>setAll_Products(data))
-
-    useEffect(()=>{
-
-            if(localStorage.getItem('auth-token')){
-                fetch('https://glow-server.onrender.com/getcart',{
-                    method:"POST",
-                    headers:{
-                        Accept:'application/json',
-                        'auth-token':`${localStorage.getItem('auth-token')}`,
-                        'Content-Type':'application/json',
-                    },
-                    body:"",
-                }).then((response)=>response.json())
-                .then((data)=>setCartItems(data))
-            }
-        }, [])
-
-
-
-    const addToCart = (itemId) => {
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-        if(localStorage.getItem('auth-token')){
-            fetch('https://glow-server.onrender.com/addtocart',{
-                method:'POST',
-                headers:{
-                    Accept:'application/json',
-                    'auth-token':`${localStorage.getItem('auth-token')}`,
-                    'Content-Type':'application/json',
-                },
-                body:JSON.stringify({"itemId":itemId}),
-            })
-            .then((response)=>response.json())
-            .then((data)=>console.log(data));
-        }
-    }
-
-
-
-    const removeFromCart = (itemId) => {
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
-        if(localStorage.getItem('auth-token')){
-            fetch('https://glow-server.onrender.com/removefromcart',{
-                method:'POST',
-                headers:{
-                    Accept:'application/json',
-                    'auth-token':`${localStorage.getItem('auth-token')}`,
-                    'Content-Type':'application/json',
-                },
-                body:JSON.stringify({"itemId":itemId}),
-            })
-            .then((response)=>response.json())
-            .then((data)=>console.log(data));
-        }
-    }
-
-
-
-
-
-    const getTotalCartAmount = () => {
-        let totalAmount = 0;
-        for (const item in cartItems) {
-            if
-            (cartItems[item] > 0) {
-                let itemInfo = all_products.find((product) => product.id === Number(item));
-                totalAmount += itemInfo.new_price * cartItems[item];
-            }
-        }
-        return totalAmount;
-    };
-
-
-
-    const getTotalCartItems = () => {
-        let totalItem = 0;
-        for(
-            const item in cartItems
-        ){
-            if(cartItems[item]>0){
-                totalItem += cartItems[item];
-            }
-        }
-        return totalItem;
-    }
-
-
-
-    const contextValue = {getTotalCartItems ,getTotalCartAmount, all_products, cartItems,addToCart,removeFromCart};
-
-
-
-    return (
-        <ShopContext.Provider value={contextValue}>
-            {props.children}
-        </ShopContext.Provider>
-    );
-
-
-
-
+  let cart = {};
+  for (let index = 0; index < 300 + 1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
 };
 
+const ShopContextProvider = (props) => {
+  const [all_products, setAll_Products] = useState([]);
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  fetch("https://glow-server.onrender.com/allproducts")
+    .then((response) => response.json())
+    .then((data) => setAll_Products(data));
+
+  useEffect(() => {
+    if (localStorage.getItem("auth-token")) {
+      fetch("https://glow-server.onrender.com/getcart", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "auth-token": `${localStorage.getItem("auth-token")}`,
+          "Content-Type": "application/json",
+        },
+        body: "",
+      })
+        .then((response) => response.json())
+        .then((data) => setCartItems(data));
+    }
+  }, []);
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    useEffect(() => {
+      if (localStorage.getItem("auth-token")) {
+        fetch("https://glow-server.onrender.com/addtocart", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "auth-token": `${localStorage.getItem("auth-token")}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ itemId: itemId }),
+        })
+          .then((response) => response.json())
+          .then((data) => console.log(data));
+      }
+    });
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    useEffect(() => {
+      if (localStorage.getItem("auth-token")) {
+        fetch("https://glow-server.onrender.com/removefromcart", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "auth-token": `${localStorage.getItem("auth-token")}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ itemId: itemId }),
+        })
+          .then((response) => response.json())
+          .then((data) => console.log(data));
+      }
+    });
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_products.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  };
+
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  };
+
+  const contextValue = {
+    getTotalCartItems,
+    getTotalCartAmount,
+    all_products,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
